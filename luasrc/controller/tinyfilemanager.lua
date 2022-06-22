88011c97f94a60bcae4139f5c2881b07da7728ed
J_88 <44608302+Jbt88@users.noreply.github.com>
2022-06-22 15:46:28 +0700

    Update

--- luasrc/controller/tinyfilemanager.lua
+++ luasrc/controller/tinyfilemanager.lua
@@ -1,4 +1,4 @@
-module("luci.controller.tinyfm", package.seeall)
+module("luci.controller.tinyfilemanager", package.seeall)
 function index()
 	entry({"admin", "system"}, firstchild(), "System", 44).dependent=false
 	entry({"admin", "system", "tinyfilemanager"}, template("tinyfilemanager"), _("File Manager"), 55).dependent=true
